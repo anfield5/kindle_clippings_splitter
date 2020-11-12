c5a6054faaa8ba965a42a7b4fab73f9e8ada37ae
anfield5 <anria.dev@gmail.com>
2020-11-12 04:39:44 +0200

    Update kindle_note_parser.py
    update script version to lates

--- kindle_note_parser.py
+++ kindle_note_parser.py
@@ -2,31 +2,40 @@
 import codecs
 import re
 import unicodedata
+from transliterate import translit, get_available_language_codes
+from langdetect import detect
 
 book_notes = {}
 bnewbook = False
 book_name = ""
-with codecs.open('My_Clippings.txt','r+','utf-8') as notes:
+log=codecs.open('log.txt','w','utf-8')
+notes=codecs.open('My_Clippings.txt','r+','utf-8')
 # find all unique book names
-    for index, line in enumerate(notes, start=1):
-        if index == 2 or bnewbook: 
-            book_name = line.upper().strip().replace(" ","_").replace(".","")
-#            book_name = re.sub('[^А-Я0-9]+', ' ', book_name)
-            book_name = re.sub(r'[\x00-\x1f\x7f-\x9f]', '', book_name)
-            book_name = book_name[:30].encode("utf-8")            
-            bnewbook = False
-        if line.strip() == '==========': 
-            bnewbook = True 
-            continue
-        if book_name!="":
-            if book_name in book_notes and book_notes[book_name]: 
-                book_notes[book_name]+=line+"\n"
-            else:
-                book_notes[book_name]=line+"\n"
+for index, line in enumerate(notes, start = 1):
+    if index == 2 or bnewbook: 
+        book_name = line.upper().strip().replace(" ","_").replace(".","")
+#        book_name = book_name.replace("/[\W_]/g", "_");
+#        book_name = translit(book_name, reversed = True)
+        book_name = book_name.encode("utf-8")
+        book_name = book_name.replace("\xef\xbb\xbf", "") 
+#        book_name = book_name[0:30]         
+        bnewbook = False
+    if line.strip() == '==========': 
+        bnewbook = True 
+        continue
+    if book_name != "":
+        if book_name in book_notes and book_notes[book_name]: 
+            book_notes[book_name] += line + "\n"            
+        else:
+            book_notes[book_name] = line + "\n"
 # for every unique book name create a file in folder books
 for book_name in book_notes:
-    if "59_СКАЗАТЬ_ЖИЗНИ_ДА!_ВИКТОР" in book_name:
-        print(str(len(book_name))+" 1"+book_name+"2")
-    book = codecs.open("books/"+book_name+".txt","w",'utf-8')
+    book_fname = "books/" + book_name + ".txt"
+    try:
+        book = codecs.open(book_fname,"w",'utf-8')
+    except:
+        print(book_name)
+        log.write(book_name.decode("utf-8")) 
+        continue
     book.write(book_notes[book_name])  
     book.close()
